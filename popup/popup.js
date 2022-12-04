da94930cda53936cc56aa009a60e7b635a89020a
Edson Junior <edsonpetrinijr@gmail.com>
2022-12-04 18:23:01 -0300

    Dark Mode

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,63 @@
+function changeTheme(oldTheme, newTheme) {
+  document.querySelector("body").classList.remove(oldTheme);
+  document.querySelector("a").classList.remove(oldTheme);
+  document.querySelector("input").classList.remove(oldTheme);
+  document
+    .getElementById("copyTextToClipboardButton")
+    .classList.remove(oldTheme);
+
+  document.querySelector("body").classList.add(newTheme);
+  document.querySelector("a").classList.add(newTheme);
+  document.querySelector("input").classList.add(newTheme);
+  document.getElementById("copyTextToClipboardButton").classList.add(newTheme);
+
+  if (oldTheme === "light") {
+    document.getElementById("theme").src = "./images/darkMode/sun.svg";
+    document.getElementById("copyTextToClipboardIcon").src =
+      "./images/darkMode/clipboard.svg";
+  } else {
+    document.getElementById("theme").src = "./images/lightMode/moon.svg";
+    document.getElementById("copyTextToClipboardIcon").src =
+      "./images/lightMode/clipboard.svg";
+  }
+}
+
+document.getElementById("theme").addEventListener("mouseover", () => {
+  chrome.storage.local.get(["newTheme"]).then(({ newTheme }) => {
+    if (newTheme === "light") {
+      document.getElementById("theme").src =
+        "./images/lightMode/moonFilled.svg";
+    } else {
+      document.getElementById("theme").src = "./images/darkMode/sunFilled.svg";
+    }
+  });
+});
+
+document.getElementById("theme").addEventListener("mouseout", () => {
+  chrome.storage.local.get(["newTheme"]).then(({ newTheme }) => {
+    if (newTheme === "light") {
+      document.getElementById("theme").src = "./images/lightMode/moon.svg";
+    } else {
+      document.getElementById("theme").src = "./images/darkMode/sun.svg";
+    }
+  });
+});
+
+// Theme
+document.getElementById("theme").addEventListener("click", () => {
+  const oldTheme = document.querySelector("body").classList.contains("light")
+    ? "light"
+    : "dark";
+
+  const newTheme = document.querySelector("body").classList.contains("light")
+    ? "dark"
+    : "light";
+
+  chrome.storage.local.set({ newTheme: newTheme, oldTheme: oldTheme });
+
+  changeTheme(oldTheme, newTheme);
+});
+
 // Website Owner Name
 document
   .getElementById("websiteOwnerNameInput")
@@ -47,6 +107,12 @@ async function copyTextToClipboard(textToCopy) {
 }
 
 window.onload = () => {
+  chrome.storage.local
+    .get(["oldTheme", "newTheme"])
+    .then(({ oldTheme, newTheme }) => {
+      changeTheme(oldTheme, newTheme);
+    });
+
   chrome.storage.local.get(["title"]).then(({ title }) => {
     document.getElementById("title").innerHTML = title ? title : "TÍTULO";
   });
